1b65d93d6df90916a778481cffde5ea0830b3bcf
schanso <jott.ess@gmx.net>
2019-05-20 11:27:00 +0200

    raise error on wrong api credentials

--- lib/fastbillr/result.rb
+++ lib/fastbillr/result.rb
@@ -3,6 +3,7 @@ module Fastbillr
 
     class << self
       def process(response)
+        raise Error, "Response body is empty (status: #{response.status}). Please verify your API credentials." unless response.body.present? || [200, 201].include?(response.status)
         parsed_body = JSON.parse(response.body)
         raise Error, parsed_body["RESPONSE"]["ERRORS"].first unless response.status == 200
         parsed_body["RESPONSE"]
